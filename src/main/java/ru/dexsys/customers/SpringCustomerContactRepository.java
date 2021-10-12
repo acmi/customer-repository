14849a0dfcc998c89365bd834b1a700ca8a43e3f
MKrasnoperov <muxajlu-4@yandex.ru>
2021-10-12 07:41:59 +0400

    completed tests

--- src/main/java/ru/dexsys/customers/SpringCustomerContactRepository.java
+++ src/main/java/ru/dexsys/customers/SpringCustomerContactRepository.java
@@ -1,83 +1,46 @@
 package ru.dexsys.customers;
 
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.*;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 public class SpringCustomerContactRepository implements CustomerContactRepository {
-  private final DataSource dataSource;
-  private final JdbcTemplate jdbcTemplate;
+  private final NamedParameterJdbcOperations jdbc;
 
   public SpringCustomerContactRepository(DataSource dataSource) {
-    this.dataSource = dataSource;
-    this.jdbcTemplate = new JdbcTemplate(dataSource);
+    this.jdbc = new NamedParameterJdbcTemplate(dataSource);
   }
 
   @Override
   public Optional<CustomerContact> findById(String id) {
-    CustomerContact customerContact = new CustomerContact();
-    Customer customer = new Customer();
-    try (Connection connection = dataSource.getConnection()) {
-      try (PreparedStatement statement = connection.prepareStatement("select * from CUSTOMER_CONTACT 'cc' join CUSTOMER c on (cc.CUSTOMER_ID = c.ID) where cc.id = ?")) {
-        statement.setString(1, id);
-        try (ResultSet resultSet = statement.executeQuery()) {
-          if (resultSet.next()) {
-            customerContact.setId(resultSet.getString("ID"));
-            customerContact.setContact(resultSet.getString("CONTACT"));
-            customerContact.setType(CustomerContact.Type.valueOf(resultSet.getString("TYPE")));
-            customer.setId(resultSet.getString("ID"));
-            customer.setName(resultSet.getString("NAME"));
-            customerContact.setUser(findCustomer(resultSet.getString("CUSTOMER_ID")));
-          }
-        } catch (SQLException throwables) {
-          throwables.printStackTrace();
-        }
-      }
-    } catch (SQLException throwables) {
-      throwables.printStackTrace();
-    }
-    return Optional.of(customerContact);
+    return jdbc.query("select customer_contact.id AS contact_id, customer_contact.contact, " +
+        "customer_contact.type, customer_contact.customer_id, customer.name " +
+        "from customer_contact " +
+        "LEFT OUTER JOIN CUSTOMER ON customer_contact.customer_id = customer.id " +
+        "where customer_contact.id = :id", new MapSqlParameterSource("id", id), new CustomerContactResultSet()).stream().findAny();
   }
 
   @Override
   public List<CustomerContact> findAll() {
-    throw new UnsupportedOperationException("NOT IMPLEMENTED"); //TODO
+    return jdbc.query("select id, name from CUSTOMER", new CustomerContactResultSet());
   }
 
   @Override
   public CustomerContact save(CustomerContact entity) {
-    throw new UnsupportedOperationException("NOT IMPLEMENTED"); //TODO
+    jdbc.update("INSERT INTO customer_contact (id, contact, type, customer_id) VALUES (:id, :contact, :type, :customer_id)", new BeanPropertySqlParameterSource(entity));
+    return entity;
   }
 
   @Override
   public void update(CustomerContact entity) {
-    throw new UnsupportedOperationException("NOT IMPLEMENTED"); //TODO
+    jdbc.update("update customer_contact set contact = :contact, type = :type where id = :id", new BeanPropertySqlParameterSource(entity));
   }
 
   @Override
   public void deleteById(String id) {
-    throw new UnsupportedOperationException("NOT IMPLEMENTED"); //TODO
-  }
-
-  private Customer findCustomer(String id) throws SQLException {
-    Customer customer = new Customer();
-    try (Connection connection = dataSource.getConnection()) {
-      try (PreparedStatement statement = connection.prepareStatement("select * from CUSTOMER where id = ?")) {
-        statement.setString(1, id);
-        try (ResultSet resultSet = statement.executeQuery()) {
-          if (resultSet.next()) {
-            customer.setId(resultSet.getString("ID"));
-            customer.setName(resultSet.getString("NAME"));
-          }
-        }
-      }
-      return customer;
-    }
+    jdbc.update("delete from customer_contact where id = :id", new MapSqlParameterSource("id", id));
   }
 }
--- src/main/java/ru/dexsys/customers/SpringCustomerRepository.java
+++ src/main/java/ru/dexsys/customers/SpringCustomerRepository.java
@@ -1,77 +1,59 @@
 package ru.dexsys.customers;
 
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.*;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 public class SpringCustomerRepository implements CustomerRepository {
-    private final DataSource dataSource;
-    private final JdbcTemplate jdbcTemplate;
+  private final NamedParameterJdbcOperations jdbc;
 
-    public SpringCustomerRepository(DataSource dataSource) {
-        this.dataSource = dataSource;
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
-    }
+  public SpringCustomerRepository(DataSource dataSource) {
+    this.jdbc = new NamedParameterJdbcTemplate(dataSource);
+  }
 
-    @Override
-    public Optional<Customer> findById(String id) {
-        Customer customer = new Customer();
-        try (Connection connection = dataSource.getConnection()) {
-            try (PreparedStatement statement = connection.prepareStatement("select * from CUSTOMER where id = ?")) {
-                statement.setString(1, id);
-                try (ResultSet resultSet = statement.executeQuery()) {
-                    if (resultSet.next()) {
-                        customer.setId(resultSet.getString("ID"));
-                        customer.setName(resultSet.getString("NAME"));
-                    }
-                } catch (SQLException throwables) {
-                    throwables.printStackTrace();
-                }
-            }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-        return Optional.of(customer);
-    }
+  @Override
+  public Optional<Customer> findById(String id) {
+    return jdbc.query("select customer.id AS customer_id, customer.name, " +
+        "customer_contact.id AS contact_id, customer_contact.contact, customer_contact.type " +
+        "from CUSTOMER " +
+        "LEFT OUTER JOIN CUSTOMER_CONTACT ON customer.id = customer_contact.customer_id " +
+        "where customer.id = :id", new MapSqlParameterSource("id", id), new CustomerResultSet()).stream().findAny();
+  }
 
-    @Override
-    public List<Customer> findAll() {
-        Customer customer = new Customer();
-        List<Customer> customerList = null;
-        try (Connection connection = dataSource.getConnection()) {
-            try (PreparedStatement statement = connection.prepareStatement("select * from CUSTOMER")) {
-                try (ResultSet resultSet = statement.executeQuery()) {
-                    if (resultSet.next()) {
-                        customer.setId(resultSet.getString("ID"));
-                        customer.setName(resultSet.getString("NAME"));
-                        customerList.add(customer);
-                    }
-                }
-            }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-        return customerList;
-    }
+  @Override
+  public List<Customer> findAll() {
+    return jdbc.query("select id, name from CUSTOMER", new CustomerResultSet());
+  }
 
-    @Override
-    public Customer save(Customer entity) {
-        throw new UnsupportedOperationException("NOT IMPLEMENTED"); //TODO
+  @Override
+  public Customer save(Customer entity) {
+    jdbc.update("insert into customer (id, name) values (:id, :name)",
+        new BeanPropertySqlParameterSource(entity));
+    if (entity.getContacts() != null) {
+      for (CustomerContact contact : entity.getContacts()) {
+        var parameterSource = new MapSqlParameterSource();
+        parameterSource.addValue("id", contact.getId());
+        parameterSource.addValue("contact", contact.getContact());
+        parameterSource.addValue("type", contact.getType().toString());
+        parameterSource.addValue("customer_id", entity.getId());
+        jdbc.update("INSERT INTO customer_contact (id, contact, type, customer_id) VALUES (:id, :contact, :type, :customer_id)",
+            parameterSource);
+      }
     }
+    return entity;
+  }
 
-    @Override
-    public void update(Customer entity) {
-        throw new UnsupportedOperationException("NOT IMPLEMENTED"); //TODO
-    }
+  @Override
+  public void update(Customer entity) {
+    SqlParameterSource namedParameters = new MapSqlParameterSource().
+        addValue("id", entity.getId()).addValue("id", entity.getName());
+    jdbc.update("update customer set name = :name where id = :id", namedParameters);
+  }
 
-    @Override
-    public void deleteById(String id) {
-        throw new UnsupportedOperationException("NOT IMPLEMENTED"); //TODO
-    }
+  @Override
+  public void deleteById(String id) {
+    jdbc.update("delete from customer where id = :id", new MapSqlParameterSource("id", id));
+  }
 }
