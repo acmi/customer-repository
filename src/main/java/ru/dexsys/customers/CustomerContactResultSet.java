8620fcd28d5e627b0c23f4288f5ec85025b0dc3f
Valerii Glavatskih <glavatskihvalerii@yandex.ru>
2021-10-17 16:42:43 +0400

    Test Customer Seved complite.

---
+++ src/main/java/ru/dexsys/customers/CustomerContactResultSet.java
@@ -0,0 +1,37 @@
+package ru.dexsys.customers;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.ResultSetExtractor;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerContactResultSet implements ResultSetExtractor<List<CustomerContact>> {
+    @Override
+    public List<CustomerContact> extractData(ResultSet resultSet) throws SQLException, DataAccessException {
+        List<CustomerContact> customerContacts = new ArrayList<>();
+        Customer user = null;
+        while (resultSet.next()) {
+            if (user == null) {
+                user = new Customer();
+                user.setId(resultSet.getString("customer_id"));
+                user.setName("name");
+            }
+            CustomerContact customerContact = new CustomerContact();
+            customerContact.setId(resultSet.getString("contact_id"));
+            customerContact.setContact(resultSet.getString("contact"));
+            for (CustomerContact.Type b : CustomerContact.Type.values()) {
+                if (resultSet.getString("type").contains("email")) {
+                    customerContact.setType(CustomerContact.Type.EMAIL);
+                } else {
+                    customerContact.setType(CustomerContact.Type.MOBILE);
+                }
+            }
+            user.addContact(customerContact);
+            customerContacts.add(customerContact);
+        }
+        return customerContacts;
+    }
+}
--- src/main/java/ru/dexsys/customers/SpringCustomerRepository.java
+++ src/main/java/ru/dexsys/customers/SpringCustomerRepository.java
@@ -1,35 +1,67 @@
 package ru.dexsys.customers;
 
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+
 import javax.sql.DataSource;
 import java.util.List;
 import java.util.Optional;
 
 public class SpringCustomerRepository implements CustomerRepository {
+    private final NamedParameterJdbcTemplate jdbc;
+
     public SpringCustomerRepository(DataSource dataSource) {
+        this.jdbc = new NamedParameterJdbcTemplate(dataSource);
     }
 
     @Override
     public Optional<Customer> findById(String id) {
-        throw new UnsupportedOperationException("NOT IMPLEMENTED"); //TODO
+        return jdbc.query(
+                "select customer.id as customer_id, customer.name, " +
+                        "customer_contact.id as contact_id," +
+                        "customer_contact.contact," +
+                        "customer_contact.type " +
+                        "from customer " +
+                        "left outer join customer_contact on customer.id = customer_contact.customer_id " +
+                        "where customer.id = :id",
+                new MapSqlParameterSource("id", id),
+                new CustomerResultSet()).stream().findAny();
     }
 
     @Override
     public List<Customer> findAll() {
-        throw new UnsupportedOperationException("NOT IMPLEMENTED"); //TODO
+        return jdbc.query("select id, name from customer", new CustomerResultSet());
     }
 
     @Override
     public Customer save(Customer entity) {
-        throw new UnsupportedOperationException("NOT IMPLEMENTED"); //TODO
+        jdbc.update("insert into customer (id, name) values (:id, :name)",
+                new BeanPropertySqlParameterSource(entity));
+        if (entity.getContacts() != null) {
+            for (CustomerContact contact : entity.getContacts()) {
+                var parameterSource = new MapSqlParameterSource();
+                parameterSource.addValue("id", contact.getId());
+                parameterSource.addValue("contact", contact.getContact());
+                parameterSource.addValue("type", contact.getType().toString());
+                parameterSource.addValue("customer_id", entity.getId());
+                jdbc.update("insert into customer_contact (id, contact, type, customer_id) values (:id, :contact, :type, :customer_id)",
+                        parameterSource);
+            }
+        }
+        return entity;
     }
 
     @Override
     public void update(Customer entity) {
-        throw new UnsupportedOperationException("NOT IMPLEMENTED"); //TODO
+        SqlParameterSource sqlParameterSource = new MapSqlParameterSource().
+                addValue("id", entity.getId()).addValue("id", entity.getName());
+        jdbc.update("update customer set name = :name where id = :id", sqlParameterSource);
     }
 
     @Override
     public void deleteById(String id) {
-        throw new UnsupportedOperationException("NOT IMPLEMENTED"); //TODO
+        jdbc.update("delete from customer where id = :id", new MapSqlParameterSource("id", id));
     }
 }
